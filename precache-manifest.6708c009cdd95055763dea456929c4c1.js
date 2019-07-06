2b75ebf4649e23cd4c71439da78aa1c36b846526
Jorge Barbosa <jorgebarbosa_@outlook.com>
2019-07-06 11:18:32 -0400

    Updates

---
+++ precache-manifest.6708c009cdd95055763dea456929c4c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23f66c11098d49fc4c135bb4009f91d2",
+    "url": "/react-contact-manager/index.html"
+  },
+  {
+    "revision": "e10a2a9701f0b5a10e79",
+    "url": "/react-contact-manager/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "e10a2a9701f0b5a10e79",
+    "url": "/react-contact-manager/static/js/2.02db7d67.chunk.js"
+  },
+  {
+    "revision": "55b0846dc1307b76207e",
+    "url": "/react-contact-manager/static/js/main.d1fbf179.chunk.js"
+  },
+  {
+    "revision": "817909a50868ee2241bd",
+    "url": "/react-contact-manager/static/js/runtime~main.f3ab48b1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reat-contact-manager/precache-manifest.c044cb72f2aa69ba856615d810896093.js"
+  "/react-contact-manager/precache-manifest.6708c009cdd95055763dea456929c4c1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reat-contact-manager/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-contact-manager/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
