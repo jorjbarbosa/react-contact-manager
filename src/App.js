d3c2bec7225cbae49607945d9516b60c333740a6
Jorge Barbosa <jorgebarbosa_@outlook.com>
2019-07-06 11:00:16 -0400

    Edit Contact

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import Header from './components/layout/Header';
 import { Provider } from './context';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AddContact from './components/contacts/AddContact';
+import EditContact from './components/contacts/EditContact';
 import About from './components/pages/About';
 import NotFound from './components/pages/NotFound';
+
 class App extends React.Component {
   render() {
     return (
@@ -18,6 +20,7 @@ class App extends React.Component {
               <Switch>
                 <Route exact path="/" component={Contacts} />
                 <Route exact path="/contact/add" component={AddContact} />
+                <Route exact path="/contact/edit/:id" component={EditContact} />
                 <Route exact path="/about" component={About} />
                 <Route component={NotFound} />
               </Switch>
