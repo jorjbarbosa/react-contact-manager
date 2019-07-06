c72050c2a127fbb51558740faebbb26bedc796b6
Jorge Barbosa <jorgebarbosa_@outlook.com>
2019-07-06 11:12:12 -0400

    Updates

---
+++ precache-manifest.d40b0f707663a08684ac4ab0706dba54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9e6e38de8b738db6276e6d7451569ae",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "e10a2a9701f0b5a10e79",
+    "url": "/contactmanager/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "e10a2a9701f0b5a10e79",
+    "url": "/contactmanager/static/js/2.02db7d67.chunk.js"
+  },
+  {
+    "revision": "07732c6396bc24829ecf",
+    "url": "/contactmanager/static/js/main.c5f0cc2d.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
